6564ad349ee8b84cd85d265336a418fe25345d2e
Ayush Mittal <ayush.mittal@outlook.com>
2022-05-22 10:18:03 +0530

    some changes

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,40 @@ var router = express.Router();
 module.exports = function (app) {
   /* GET home page. */
   router.get("/", function (req, res, next) {
-    res.render("index", { title: "Express" });
+    res.render("issues", {});
   });
 
   router.get("/issues", (req, res, next) => {
+    const { page = 1, per_page = 20 } = req.query || {};
     var config = {
       method: "get",
-      url: `https://gitlab.com/api/v4/projects/${process.env.GITLAB_PROJECT_ID}/issues`,
+      url: `https://gitlab.com/api/v4/projects/${process.env.GITLAB_PROJECT_ID}/issues?page=${page}&per_page=${per_page}`,
       headers: {
         Authorization: `Bearer ${process.env.GITLAB_PERSONAL_ACCESS_TOKEN}`,
       },
     };
+    console.log(config);
 
     axios(config)
       .then(function (response) {
+        console.log(response);
         // console.log(JSON.stringify(response.data));
-        res.render("issues", { issues: response.data });
+        // res.render("issues", { issues: response.data });
+        const { headers } = response;
+        const totalPages = headers["x-total-pages"];
+        const totalIssues = headers["x-total"];
+        // const totalPage = headers["x-total-pages"];
+        return res.status(200).json({
+          error: false,
+          data: { issues: response.data, totalPages, totalIssues },
+        });
       })
       .catch(function (error) {
         console.log(error);
+        return res.status(400).json({
+          error: true,
+          messgae: "Something went wrong",
+        });
       });
   });
 
