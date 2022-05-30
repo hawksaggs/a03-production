6fc569f9a6e5c0f092faa63097e0623971548fa4
Ayush Mittal <ayush.mittal@outlook.com>
2022-05-30 09:50:19 +0530

    added js and css file in public

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.set("view engine", "hbs");
 
 app.use(logger("dev"));
 app.use(cors());
-app.use(helmet());
+app.use(helmet({contentSecurityPolicy: false}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
